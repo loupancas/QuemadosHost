ff38499ef3af6b7f86e97f91014bd2b7fe43d3f6
loupancas <129621626+loupancas@users.noreply.github.com>
2024-06-25 01:11:26 -0500

    Reapply "commits"
    This reverts commit 8fdb2cb84b1581e00a94dc758b7405f07c9900e1.

--- Assets/Scripts/Game/UIGameOverView.cs
+++ Assets/Scripts/Game/UIGameOverView.cs
@@ -5,52 +5,31 @@ using UnityEngine;
 
 	public class UIGameOverView : MonoBehaviour
 	{
-		public TextMeshProUGUI Winner;
 		public GameObject      Victory;
 		public GameObject      Defeat;
 
-		private GameUI _gameUI;
-		private EGameplayState _lastState;
+        LifeHandler _lifeHandler;
 
-		
-
-		private void Awake()
-		{
-			_gameUI = GetComponentInParent<GameUI>();
-		}
-
-		private void Update()
-		{
-			if (_gameUI.Runner == null)
-				return;
+        private void Update()
+		{		
 
 			// Unlock cursor.
 			Cursor.lockState = CursorLockMode.None;
-			Cursor.visible = true;
+			Cursor.visible = true;      
 
-			if (_gameUI.Gameplay.Object == null || _gameUI.Gameplay.Object.IsValid == false)
-				return;
-
-			if (_lastState == _gameUI.Gameplay.State)
-				return;
+   //        if (bool localPlayerIsWinner = false)
+		 //  {
+			//Defeat.SetActive(localPlayerIsWinner == true);
 
 
-			_lastState = _gameUI.Gameplay.State;
+   //        }
+		 //  else
+   //        {
+   //         Victory.SetActive(localPlayerIsWinner == true);
+   //        }
 
-			bool localPlayerIsWinner = false;
-			Winner.text = string.Empty;
+        }
 
-			foreach (var playerPair in _gameUI.Gameplay.PlayerData)
-			{
-				//if (playerPair.Value.StatisticPosition != 1)
-				//	continue;
 
-				Winner.text = $"Winner is {playerPair.Value.Nickname}";
-				localPlayerIsWinner = playerPair.Key == _gameUI.Runner.LocalPlayer;
-			}
-
-			Victory.SetActive(localPlayerIsWinner);
-			Defeat.SetActive(localPlayerIsWinner == false);
-		}
-	}
+    }
 
